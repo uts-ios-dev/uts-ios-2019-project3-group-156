2a0fde613efed2551c54f95fb0eeec4b1baf5d58
TheronAnn <48783154+TheronAnn@users.noreply.github.com>
2019-05-22 17:00:44 +1000

    Add files via upload
    Initial Project

---
+++ Focus booster/Focus booster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Focus booster
+//
+//  Created by Theron Ann on 22/5/19.
+//  Copyright © 2019 Theron Ann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
