0eb9ee9b86a4e756e3aa54746306d92f7b2ba159
lvino <lyuniancheng@gamil.com>
2019-06-01 23:44:45 +1000

    save local

--- Focus booster/Focus booster/OverviewViewController.swift
+++ Focus booster/Focus booster/OverviewViewController.swift
@@ -37,12 +37,12 @@ class OverviewViewController: UIViewController {
         let entry1 = BarChartDataEntry(x: 1.0, y: Double(number1.value))
         let entry2 = BarChartDataEntry(x: 2.0, y: Double(number2.value))
         let entry3 = BarChartDataEntry(x: 3.0, y: Double(number3.value))
-        let dataSet = BarChartDataSet(entries: [entry1, entry2, entry3], label: "Widgets Type")
+        let dataSet = BarChartDataSet(entries: [entry1, entry2, entry3], label: "Types")
         print(dataSet)
         dataSet.colors = ChartColorTemplates.vordiplom()
         let data = BarChartData(dataSets: [dataSet])
         self.barChart.data = data
-        barChart.chartDescription?.text = "Number of Widgets by Type"
+        barChart.chartDescription?.text = "Number of Types"
         
         //All other additions to this function will go here
         
@@ -54,7 +54,7 @@ class OverviewViewController: UIViewController {
         let entry1 = PieChartDataEntry(value: Double(number1.value), label: "#1")
         let entry2 = PieChartDataEntry(value: Double(number2.value), label: "#2")
         let entry3 = PieChartDataEntry(value: Double(number3.value), label: "#3")
-        let dataSet = PieChartDataSet(entries: [entry1, entry2, entry3], label: "Widget Types")
+        let dataSet = PieChartDataSet(entries: [entry1, entry2, entry3], label: "Types")
         dataSet.colors = ChartColorTemplates.joyful()
         //dataSet.valueColors = [UIColor.black]
         pieChart.backgroundColor = UIColor.black
@@ -70,7 +70,7 @@ class OverviewViewController: UIViewController {
         
         let data = PieChartData(dataSet: dataSet)
         pieChart.data = data
-        pieChart.chartDescription?.text = "Share of Widgets by Type"
+        pieChart.chartDescription?.text = "Task Type"
         
         //All other additions to this function will go here
         
